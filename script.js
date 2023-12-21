ab7b2f837ed90d01bcfccb3797d8ebfedd795cfa
om <omkumar1892002@gmail.com>
2023-12-22 02:03:58 +0530

    Added bars on display with their random generating effect

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("I am in the javascript Console.")
 
-let n = 10;
+let n = 50;
 const arr=[];
 let container = document.querySelector(".container");
 
@@ -8,9 +8,11 @@ for(let i = 0; i < n; i++){
     arr[i] = Math.random();
 }
 
+// Below is showing our bars on display.
 for(let i = 0; i < n; i++){
     let bar = document.createElement("div")
-    bar.style.height = arr[i] * 100 + "%"
+    bar.style.height = arr[i] * 80 + "%"
+    bar.style.width = "20px"
     bar.classList.add("bar");
     container.appendChild(bar);
 }
