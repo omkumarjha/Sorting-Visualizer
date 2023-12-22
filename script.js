4906009355c84593d2c1c1b242c297e5e707b1bd
om <omkumar1892002@gmail.com>
2023-12-22 23:37:04 +0530

    Added logic to visualize bubble sort using JS

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
-console.log("I am in the javascript Console.")
-
 let n = 50;
 const arr=[];
 let container = document.querySelector(".container");
 
-for(let i = 0; i < n; i++){
-    arr[i] = Math.random();
+init(); // This function is just to initalize the bars with random heights and later we will apply sorting .
+function init(){
+    for(let i = 0; i < n; i++){
+        arr[i] = Math.random();
+    }
+    showBars();
 }
 
-// Below is showing our bars on display.
-for(let i = 0; i < n; i++){
-    let bar = document.createElement("div")
-    bar.style.height = arr[i] * 80 + "%"
-    bar.style.width = "20px"
-    bar.classList.add("bar");
-    container.appendChild(bar);
+// Its just swapping two values in an array .
+function swap(a, b,copy) {
+    let temp = copy[a];
+    copy[a] = copy[b];
+    copy[b] = temp;
+}
+
+function animate(swaps){
+    if(swaps.length == 0){
+        showBars()
+    }
+    else{
+        const [i,j] = swaps.shift();
+
+        swap(i,j,arr);
+        showBars([i,j]);
+        setTimeout(function(){
+            animate(swaps);
+        },10)
+    }
+}
+
+
+function bubbleSort(){
+    const swaps = [];
+    const copy = [...arr];
+    console.log("yes")
+
+    for(let i = 1; i <= (n-1); i++){
+        for(let j = 0; j < (n-i); j++){
+            if(copy[j] > copy[j+1]){
+                swaps.push([j,j+1]);
+                swap(j,j+1,copy);
+            }
+        }
+    }
+    animate(swaps)
+    showBars()
+}
+
+// Below is showing the bars on screen.
+function showBars(indices){
+    container.innerHTML = "";
+    for(let i = 0; i < n; i++){
+        let bar = document.createElement("div")
+        bar.style.height = arr[i] * 80 + "%"
+        bar.style.width = "20px"
+        bar.classList.add("bar");
+        if(indices && indices.includes(i)){
+            bar.style.backgroundColor = "red";
+        }
+        container.appendChild(bar);
+    }
 }
